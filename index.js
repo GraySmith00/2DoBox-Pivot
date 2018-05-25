f5dfd4309fd092910a59302a2c4f508508f6e920
Gray Smith <gray.smith00@gmail.com>
2018-05-25 17:00:30 -0600

    Added delete functionality and tied card DOM elements to array

--- index.js
+++ index.js
@@ -1,118 +1,120 @@
-$(document).ready(function(){
-// ===================================================================
-// VARIABLES
-// ===================================================================    
-var titleInput = $('#title-input');
-var bodyInput = $('#body-input');
-var numCards = 0;
-var qualityVariable = "swill";
+$(document).ready(function () {
+    // ===================================================================
+    // VARIABLES
+    // ===================================================================    
+    var titleInput = $('#title-input');
+    var bodyInput = $('#body-input');
+    var numCards = 0;
+    var qualityVariable = "swill";
 
-var objects = JSON.parse(localStorage.getItem('objects')) || [];
+    var objects = JSON.parse(localStorage.getItem('objects')) || [];
 
-renderAllObjects();
+    renderAllObjects();
 
 
-// ===================================================================
-// EVENT LISTENERS
-// ===================================================================
-$('#todo-form').submit(function (event) {
-    event.preventDefault();
-    if ($('#title-input').val() === "" || $('#body-input').val() === "") {
-        alert('Missing some inputs!');
-        return;
-    };
-    var object = {
-        id: numCards,
-        title: titleInput.val(),
-        body: bodyInput.val(),
-        quality: 'swill'
+    // ===================================================================
+    // EVENT LISTENERS
+    // ===================================================================
+    $('#todo-form').submit(function (event) {
+        event.preventDefault();
+        if ($('#title-input').val() === "" || $('#body-input').val() === "") {
+            alert('Missing some inputs!');
+            return;
+        };
+        var object = {
+            id: numCards,
+            title: titleInput.val(),
+            body: bodyInput.val(),
+            quality: 'swill'
+        }
+        objects.push(object);
+        numCards++;
+        $(".bottom-box").prepend(newCardHTML(object, objects.length - 1));
+        localStorage.setItem("objects", JSON.stringify(objects));
+        this.reset();
+    });
+
+
+    $('.bottom-box').on('click', '.delete-button', deleteCard);
+
+
+    // ===================================================================
+    // FUNCTIONS
+    // ===================================================================
+
+    function renderAllObjects() {
+        $('.bottom-box').html('');
+        objects.forEach(function (object, i) {
+            $(".bottom-box").prepend(newCardHTML(object, i));
+        })
     }
-    objects.push(object);
-    numCards++;
-    $(".bottom-box").prepend(newCardHTML(object));
-    console.log(objects);
-    localStorage.setItem("objects", JSON.stringify(objects));
-    this.reset();
-});
 
+    function newCardHTML(todoObject, i) {
+        return '<div data-index="' + i + '"class="card-container"><h2 class="title-of-card">'
+            + todoObject.title + '</h2>'
+            + '<button class="delete-button"></button>'
+            + '<p class="body-of-card">'
+            + todoObject.body + '</p>'
+            + '<button class="upvote"></button>'
+            + '<button class="downvote"></button>'
+            + '<p class="quality">' + 'quality:' + '<span class="qualityVariable">' + todoObject.quality + '</span>' + '</p>'
+            + '<hr>'
+            + '</div>';
+    };
 
-$('.bottom-box').on('click', deleteCard);
-
-
-// ===================================================================
-// FUNCTIONS
-// ===================================================================
-
-function renderAllObjects(){
-    objects.forEach(function(object){
-        $(".bottom-box").prepend(newCardHTML(object));
-    })
-}
-
-function newCardHTML(todoObject) {
-    return '<div id="' + todoObject.id + '"class="card-container"><h2 class="title-of-card">'
-        + todoObject.title + '</h2>'
-        + '<button class="delete-button"></button>'
-        + '<p class="body-of-card">'
-        + todoObject.body + '</p>'
-        + '<button class="upvote"></button>'
-        + '<button class="downvote"></button>'
-        + '<p class="quality">' + 'quality:' + '<span class="qualityVariable">' + todoObject.quality + '</span>' + '</p>'
-        + '<hr>'
-        + '</div>';
-};
-
-function deleteCard(event){
-    if (event.target.className === "delete-button") {
-        var cardHTML = $(event.target).closest('.card-container').remove();
-        console.log(cardHTML);
+    function deleteCard(e) {
+        var index = e.target.parentElement.dataset.index;
+        objects.splice(index, 1);
+        localStorage.setItem("objects", JSON.stringify(objects));
+        renderAllObjects();
+        // $(e.target).closest('.card-container').remove();
+        //var cardHTML = $(e.target).closest('.card-container').remove();
         // var cardHTMLId = cardHTML[0].id;
         // localStorage.removeItem(cardHTMLId);
     }
-}
 
 
-$(".bottom-box").on('click', function (event) {
-    var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
-    var qualityVariable;
+    $(".bottom-box").on('click', function (event) {
+        var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
+        var qualityVariable;
 
-    if (event.target.className === "upvote" || event.target.className === "downvote") {
+        if (event.target.className === "upvote" || event.target.className === "downvote") {
 
-        if (event.target.className === "upvote" && currentQuality === "plausible") {
-            qualityVariable = "genius";
-            $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+            if (event.target.className === "upvote" && currentQuality === "plausible") {
+                qualityVariable = "genius";
+                $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
 
-        } else if (event.target.className === "upvote" && currentQuality === "swill") {
-            qualityVariable = "plausible";
-            $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+            } else if (event.target.className === "upvote" && currentQuality === "swill") {
+                qualityVariable = "plausible";
+                $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
 
-        } else if (event.target.className === "downvote" && currentQuality === "plausible") {
-            qualityVariable = "swill"
-            $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+            } else if (event.target.className === "downvote" && currentQuality === "plausible") {
+                qualityVariable = "swill"
+                $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
 
-        } else if (event.target.className === "downvote" && currentQuality === "genius") {
-            qualityVariable = "plausible"
-            $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+            } else if (event.target.className === "downvote" && currentQuality === "genius") {
+                qualityVariable = "plausible"
+                $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
 
-        } else if (event.target.className === "downvote" && currentQuality === "swill") {
-            qualityVariable = "swill";
+            } else if (event.target.className === "downvote" && currentQuality === "swill") {
+                qualityVariable = "swill";
 
-        } else if (event.target.className === "upvote" && currentQuality === "genius") {
-            qualityVariable = "genius";
+            } else if (event.target.className === "upvote" && currentQuality === "genius") {
+                qualityVariable = "genius";
+            }
+
+            var cardHTML = $(event.target).closest('.card-container');
+            var cardHTMLId = cardHTML[0].id;
+            var cardObjectInJSON = localStorage.getItem(cardHTMLId);
+            var cardObjectInJS = JSON.parse(cardObjectInJSON);
+
+            cardObjectInJS.quality = qualityVariable;
+
+            var newCardJSON = JSON.stringify(cardObjectInJS);
+            localStorage.setItem(cardHTMLId, newCardJSON);
         }
 
-        var cardHTML = $(event.target).closest('.card-container');
-        var cardHTMLId = cardHTML[0].id;
-        var cardObjectInJSON = localStorage.getItem(cardHTMLId);
-        var cardObjectInJS = JSON.parse(cardObjectInJSON);
-
-        cardObjectInJS.quality = qualityVariable;
-
-        var newCardJSON = JSON.stringify(cardObjectInJS);
-        localStorage.setItem(cardHTMLId, newCardJSON);
-    }
-
-});
+    });
 
 }) //close document ready
 
