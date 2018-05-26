1be08e34e0c732072945a48f9e9b0c1e2df01b11
Gray Smith <gray.smith00@gmail.com>
2018-05-26 10:55:04 -0600

    Change javascript spacing

--- index.js
+++ index.js
@@ -1,203 +1,234 @@
-$(document).ready(function () {
-    // ===================================================================
-    // VARIABLES
-    // ===================================================================    
-    var titleInput = $('#title-input');
-    var bodyInput = $('#body-input');
-    var numCards = 0;
-    // var qualityVariable = "swill";
+$(document).ready(function() {
+  // ===================================================================
+  // VARIABLES
+  // ===================================================================
+  var numCards = 0;
+  // var qualityVariable = "swill";
 
-    var objects = JSON.parse(localStorage.getItem('objects')) || [];
+  var objects = JSON.parse(localStorage.getItem('objects')) || [];
 
-    renderAllObjects();
+  renderAllObjects();
 
-
-    // ===================================================================
-    // EVENT LISTENERS
-    // ===================================================================
-    $('#todo-form').submit(function (event) {
-        event.preventDefault();
-        if ($('#title-input').val() === "") {
-            alert('Missing a title!');
-            return;
-        }
-        if ($('#body-input').val() === "") {
-            alert('Missing a body!');
-            return;
-        }
-        var object = {
-            id: numCards,
-            title: titleInput.val(),
-            body: bodyInput.val(),
-            quality: 'swill'
-        }
-        objects.push(object);
-        numCards++;
-        $(".bottom-box").prepend(newCardHTML(object, objects.length - 1));
-        localStorage.setItem("objects", JSON.stringify(objects));
-        this.reset();
-    });
-
-    $('#title-input').on('keyup', buttonDisabled);
-    $('#body-input').on('keyup', buttonDisabled);
-
-    $('.bottom-box').on('click', '.delete-button', deleteCard);
-    $(".bottom-box").on('click', upVote);
-    $(".bottom-box").on('click', downVote);
-    $('.bottom-box').on('click', getContentEditIndex);
-    $('.bottom-box').on('keydown', function (e) {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            saveBodyEdit(e)
-        }
-    });
-    $('.bottom-box').on('focusout', saveBodyEdit);
-
-
-    // ===================================================================
-    // FUNCTIONS
-    // ===================================================================
-    function buttonDisabled() {
-        if ($('#title-input').val() !== "" || $('#body-input').val() !== "") {
-            $('.save-btn').prop('disabled', false);
-        } else {
-            $('.save-btn').prop('disabled', true);
-        };
-
+  // ===================================================================
+  // EVENT LISTENERS
+  // ===================================================================
+  $('#todo-form').submit(function(event) {
+    event.preventDefault();
+    if ($('#title-input').val() === '') {
+      alert('Missing a title!');
+      return;
     }
-
-    function renderAllObjects() {
-        $('.bottom-box').html('');
-        objects.forEach(function (object, i) {
-            $(".bottom-box").prepend(newCardHTML(object, i));
-        })
+    if ($('#body-input').val() === '') {
+      alert('Missing a body!');
+      return;
     }
-
-    function newCardHTML(todoObject, i) {
-        return '<div data-index="' + i + '"class="card-container"><h2 contenteditable="true" class="title-of-card">'
-            + todoObject.title + '</h2>'
-            + '<button class="delete-button"></button>'
-            + '<p contenteditable="true" class="body-of-card">'
-            + todoObject.body + '</p>'
-            + '<button class="upvote"></button>'
-            + '<button class="downvote"></button>'
-            + '<p class="quality">' + 'quality:' + '<span class="qualityVariable">' + todoObject.quality + '</span>' + '</p>'
-            + '<hr>'
-            + '</div>';
+    var object = {
+      id: numCards,
+      title: $('#title-input').val(),
+      body: $('#body-input').val(),
+      quality: 'swill'
     };
+    objects.push(object);
+    numCards++;
+    $('.bottom-box').prepend(newCardHTML(object, objects.length - 1));
+    localStorage.setItem('objects', JSON.stringify(objects));
+    this.reset();
+  });
 
-    function deleteCard(e) {
-        var index = e.target.parentElement.dataset.index;
-        objects.splice(index, 1);
-        localStorage.setItem("objects", JSON.stringify(objects));
-        renderAllObjects();
-        // $(e.target).closest('.card-container').remove();
-        //var cardHTML = $(e.target).closest('.card-container').remove();
-        // var cardHTMLId = cardHTML[0].id;
-        // localStorage.removeItem(cardHTMLId);
+  $('#title-input').on('keyup', buttonDisabled);
+  $('#body-input').on('keyup', buttonDisabled);
+
+  $('.bottom-box').on('click', '.delete-button', deleteCard);
+  $('.bottom-box').on('click', upVote);
+  $('.bottom-box').on('click', downVote);
+  $('.bottom-box').on('click', getContentEditIndex);
+  $('.bottom-box').on('keydown', function(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      saveBodyEdit(e);
     }
+  });
+  $('.bottom-box').on('focusout', saveBodyEdit);
 
-    function upVote(e) {
-        var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
-        var index = e.target.parentElement.dataset.index;
-        if (e.target.className === "upvote") {
-            if (currentQuality === "swill") {
-                objects[index].quality = "plausible";
-                localStorage.setItem("objects", JSON.stringify(objects));
-                $($(event.target).siblings('p.quality').children()[0]).text('plausible');
-            } else if (currentQuality === "plausible") {
-                objects[index].quality = "genius";
-                localStorage.setItem("objects", JSON.stringify(objects));
-                $($(event.target).siblings('p.quality').children()[0]).text('genius');
-            }
-        }
+  // ===================================================================
+  // FUNCTIONS
+  // ===================================================================
+  function buttonDisabled() {
+    if ($('#title-input').val() !== '' || $('#body-input').val() !== '') {
+      $('.save-btn').prop('disabled', false);
+    } else {
+      $('.save-btn').prop('disabled', true);
     }
+  }
 
-    function downVote(e) {
-        var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
-        var index = e.target.parentElement.dataset.index;
-        if (e.target.className === "downvote") {
-            if (currentQuality === "plausible") {
-                objects[index].quality = "swill";
-                localStorage.setItem('objects', JSON.stringify(objects));
-                $($(event.target).siblings('p.quality').children()[0]).text('swill');
-            } else if (currentQuality === "genius") {
-                objects[index].quality = "plausible";
-                localStorage.setItem('objects', JSON.stringify(objects));
-                $($(event.target).siblings('p.quality').children()[0]).text('plausible');
-            }
-        }
-    }
+  function renderAllObjects() {
+    $('.bottom-box').html('');
+    objects.forEach(function(object, i) {
+      $('.bottom-box').prepend(newCardHTML(object, i));
+    });
+  }
 
-    var contentEditIndex;
+  function newCardHTML(todoObject, i) {
+    return (
+      '<div data-index="' +
+      i +
+      '"class="card-container"><h2 contenteditable="true" class="title-of-card">' +
+      todoObject.title +
+      '</h2>' +
+      '<button class="delete-button"></button>' +
+      '<p contenteditable="true" class="body-of-card">' +
+      todoObject.body +
+      '</p>' +
+      '<button class="upvote"></button>' +
+      '<button class="downvote"></button>' +
+      '<p class="quality">' +
+      'quality:' +
+      '<span class="qualityVariable">' +
+      todoObject.quality +
+      '</span>' +
+      '</p>' +
+      '<hr>' +
+      '</div>'
+    );
+  }
 
-    function getContentEditIndex(e) {
-        contentEditIndex = e.target.parentElement.dataset.index;
-    }
-
-    function saveBodyEdit(e) {
-        if (e.target.nodeName === 'P') {
-            if (objects[contentEditIndex].body !== e.target.innerText) {
-                objects[contentEditIndex].body = e.target.innerText;
-                localStorage.setItem('objects', JSON.stringify(objects));
-            }
-        } else if (e.target.nodeName === 'H2') {
-            if (objects[contentEditIndex].title !== e.target.innerText) {
-                objects[contentEditIndex].title = e.target.innerText;
-                localStorage.setItem('objects', JSON.stringify(objects));
-            }
-        }
-    }
-
-    // $(".bottom-box").on('click', function (event) {
-    //     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
-    //     var qualityVariable;
-
-    //     if (event.target.className === "upvote" || event.target.className === "downvote") {
-
-    //         if (event.target.className === "upvote" && currentQuality === "plausible") {
-    //             qualityVariable = "genius";
-    //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-    //         } else if (event.target.className === "upvote" && currentQuality === "swill") {
-    //             qualityVariable = "plausible";
-    //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-    //         } else if (event.target.className === "downvote" && currentQuality === "plausible") {
-    //             qualityVariable = "swill"
-    //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-    //         } else if (event.target.className === "downvote" && currentQuality === "genius") {
-    //             qualityVariable = "plausible"
-    //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-    //         } else if (event.target.className === "downvote" && currentQuality === "swill") {
-    //             qualityVariable = "swill";
-
-    //         } else if (event.target.className === "upvote" && currentQuality === "genius") {
-    //             qualityVariable = "genius";
-    //         }
-
-    // var cardHTML = $(event.target).closest('.card-container');
+  function deleteCard(e) {
+    var index = e.target.parentElement.dataset.index;
+    objects.splice(index, 1);
+    localStorage.setItem('objects', JSON.stringify(objects));
+    renderAllObjects();
+    // $(e.target).closest('.card-container').remove();
+    //var cardHTML = $(e.target).closest('.card-container').remove();
     // var cardHTMLId = cardHTML[0].id;
-    // var cardObjectInJSON = localStorage.getItem(cardHTMLId);
-    // var cardObjectInJS = JSON.parse(cardObjectInJSON);
+    // localStorage.removeItem(cardHTMLId);
+  }
 
-    // cardObjectInJS.quality = qualityVariable;
+  function upVote(e) {
+    var currentQuality = $(
+      $(event.target)
+        .siblings('p.quality')
+        .children()[0]
+    )
+      .text()
+      .trim();
+    var index = e.target.parentElement.dataset.index;
+    if (e.target.className === 'upvote') {
+      if (currentQuality === 'swill') {
+        objects[index].quality = 'plausible';
+        localStorage.setItem('objects', JSON.stringify(objects));
+        $(
+          $(event.target)
+            .siblings('p.quality')
+            .children()[0]
+        ).text('plausible');
+      } else if (currentQuality === 'plausible') {
+        objects[index].quality = 'genius';
+        localStorage.setItem('objects', JSON.stringify(objects));
+        $(
+          $(event.target)
+            .siblings('p.quality')
+            .children()[0]
+        ).text('genius');
+      }
+    }
+  }
 
-    // var newCardJSON = JSON.stringify(cardObjectInJS);
-    // localStorage.setItem(cardHTMLId, newCardJSON);
-    //     }
+  function downVote(e) {
+    var currentQuality = $(
+      $(event.target)
+        .siblings('p.quality')
+        .children()[0]
+    )
+      .text()
+      .trim();
+    var index = e.target.parentElement.dataset.index;
+    if (e.target.className === 'downvote') {
+      if (currentQuality === 'plausible') {
+        objects[index].quality = 'swill';
+        localStorage.setItem('objects', JSON.stringify(objects));
+        $(
+          $(event.target)
+            .siblings('p.quality')
+            .children()[0]
+        ).text('swill');
+      } else if (currentQuality === 'genius') {
+        objects[index].quality = 'plausible';
+        localStorage.setItem('objects', JSON.stringify(objects));
+        $(
+          $(event.target)
+            .siblings('p.quality')
+            .children()[0]
+        ).text('plausible');
+      }
+    }
+  }
 
-    // });
+  var contentEditIndex;
 
-}) //close document ready
+  function getContentEditIndex(e) {
+    contentEditIndex = e.target.parentElement.dataset.index;
+  }
 
+  function saveBodyEdit(e) {
+    if (e.target.nodeName === 'P') {
+      if (objects[contentEditIndex].body !== e.target.innerText) {
+        objects[contentEditIndex].body = e.target.innerText;
+        localStorage.setItem('objects', JSON.stringify(objects));
+      }
+    } else if (e.target.nodeName === 'H2') {
+      if (objects[contentEditIndex].title !== e.target.innerText) {
+        objects[contentEditIndex].title = e.target.innerText;
+        localStorage.setItem('objects', JSON.stringify(objects));
+      }
+    }
+  }
+
+  // $(".bottom-box").on('click', function (event) {
+  //     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
+  //     var qualityVariable;
+
+  //     if (event.target.className === "upvote" || event.target.className === "downvote") {
+
+  //         if (event.target.className === "upvote" && currentQuality === "plausible") {
+  //             qualityVariable = "genius";
+  //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+
+  //         } else if (event.target.className === "upvote" && currentQuality === "swill") {
+  //             qualityVariable = "plausible";
+  //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+
+  //         } else if (event.target.className === "downvote" && currentQuality === "plausible") {
+  //             qualityVariable = "swill"
+  //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+
+  //         } else if (event.target.className === "downvote" && currentQuality === "genius") {
+  //             qualityVariable = "plausible"
+  //             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
+
+  //         } else if (event.target.className === "downvote" && currentQuality === "swill") {
+  //             qualityVariable = "swill";
+
+  //         } else if (event.target.className === "upvote" && currentQuality === "genius") {
+  //             qualityVariable = "genius";
+  //         }
+
+  // var cardHTML = $(event.target).closest('.card-container');
+  // var cardHTMLId = cardHTML[0].id;
+  // var cardObjectInJSON = localStorage.getItem(cardHTMLId);
+  // var cardObjectInJS = JSON.parse(cardObjectInJSON);
+
+  // cardObjectInJS.quality = qualityVariable;
+
+  // var newCardJSON = JSON.stringify(cardObjectInJS);
+  // localStorage.setItem(cardHTMLId, newCardJSON);
+  //     }
+
+  // });
+}); //close document ready
 
 // ===================================================================
 // GRAVEYARD
 // ===================================================================
-
 
 // function displayLocalStorage() {
 //     for (var i = 0; i < localStorage.length; i++){
@@ -206,10 +237,6 @@ $(document).ready(function () {
 //         $(".bottom-box").prepend(newCardHTML(localStorageItem));
 //     }
 // }
-
-
-
-
 
 // function localStoreCard(todoObject) {
 //     var cardString = JSON.stringify(todoObject);
@@ -223,7 +250,6 @@ $(document).ready(function () {
 //         quality: qualityVariable
 //     };
 // }
-
 
 // $.each(localStorage, function (key) {
 //     var cardData = JSON.parse(this);
